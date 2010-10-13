016500759fe3e5c764ba87b3d8da4b45413bbf1c
Fletcher Nichol <fnichol@nichol.ca>
2010-10-12 23:47:40 -0600

    Update cookbook metadata and some documentation.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,23 @@ license          "Apache 2.0"
 description      "Installs/Configures rvm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+recipe "rvm", "Installs system-wide RVM"
+recipe "rvm::rubies", "Installs RVM rubies"
+recipe "rvm::default_ruby", "Sets a default RVM ruby"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+attribute "rvm/default_ruby",
+  :display_name => "Default ruby",
+  :description => "Default ruby for new shells",
+  :default => "ree"
+
+attribute "rvm/rubies",
+  :display_name => "Installed RVM rubies",
+  :description => "Set of rubites that RVM will manage",
+  :type => "array",
+  :default => [ "ree" ]
+
