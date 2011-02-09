68a7614e59cde33ed3a38a19d95efa8a48a358fa
fnichol <fnichol@nichol.ca>
2011-02-09 16:00:29 -0700

    Remove some commenting for now.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -138,11 +138,7 @@ def install_ruby_dependencies(rubie)
     case node[:platform]
       when "centos","redhat","fedora","suse"
         pkgs = %w{ gcc-c++ patch readline readline-devel zlib zlib-devel
-                   libyaml-devel libffi-devel openssl-devel } #iconv-devel }
-        # TODO: Figure out the real state of iconv-devel and fixup the next few lines
-        # NOTE: For centos 5.4 final iconv-devel might not be available :(
-        #pkgs.delete('iconv-devel') if (node[:platform] == "centos" &&
-        #                                !node[:platform_version] == '5.5')
+                   libyaml-devel libffi-devel openssl-devel }
         pkgs << %w{ git subversion autoconf } if rubie =~ /^ruby-head$/
       when "debian","ubuntu"
         pkgs = %w{ build-essential bison openssl libreadline6 libreadline6-dev
