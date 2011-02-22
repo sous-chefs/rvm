5711b3d8c0d4a99b31728f44398fdcaec6544fc2
Fletcher Nichol <fnichol@nichol.ca>
2011-02-22 00:35:09 -0700

    Bump rvm default to 1.9.2-p180.

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,7 +24,7 @@ default[:rvm][:group_users] = []
 default[:rvm][:rvmrc] = Hash.new
 
 # ruby that will get set to `rvm use default`. Use fully qualified ruby names.
-default[:rvm][:default_ruby] = "ruby-1.9.2-p136"
+default[:rvm][:default_ruby] = "ruby-1.9.2-p180"
 
 # list of rubies that will be installed
 default[:rvm][:rubies] = [ select_ruby(rvm[:default_ruby]) ]
