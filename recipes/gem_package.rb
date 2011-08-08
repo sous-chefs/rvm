0df34e8d5808edbdf18d943cebed183218e6ab0d
Fletcher Nichol <fnichol@nichol.ca>
2011-08-08 14:33:11 -0600

    Refactor gem_package provider.

--- recipes/gem_package.rb
+++ recipes/gem_package.rb
@@ -17,14 +17,17 @@
 # limitations under the License.
 #
 
-rvm_tmp = node['rvm']['gem_package']['rvm_string']
-case rvm_tmp
-when String; rvm_descriptor = rvm_tmp + " RVM Ruby"
+node_val = node['rvm']['gem_package']['rvm_string']
+case node_val
+when String
+  rvm_descriptor = node_val + " RVM Ruby"
 when Array
-  last = rvm_tmp.pop()
-  rvm_descriptor = [ rvm_tmp.join(', '), last ].join(' & ') + " RVM Rubies"
+  last = node_val.pop
+  rvm_descriptor = [ node_val.join(', '), last ].join(' & ') + " RVM Rubies"
 end
+
 patch_gem_package
+
 ::Chef::Log.info "gem_package resource has been patched to use provider " <<
   "Chef::Provider::Package::RVMRubygems and will install gems to " <<
   "the #{rvm_descriptor}."
