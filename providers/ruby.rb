4648849928c80828067bc0a2c387a46df8d6ed68
Fletcher Nichol <fnichol@nichol.ca>
2011-08-13 22:10:01 -0600

    Ubuntu doesn't have an ncurses-dev pkg.

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -124,7 +124,6 @@ def install_ruby_dependencies(rubie)
                    zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0
                    libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf
                    libc6-dev ssl-cert }
-        pkgs += %w{ ncurses-dev } if node['platform'] == 'ubuntu'
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
         pkgs = %w{ gcc-c++ patch zlib zlib-devel libffi-devel
