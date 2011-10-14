99a2a3036e7790e08041c215bc7452542ad2b07b
Ryan Schlesinger <ryan@instanceinc.com>
2011-10-13 22:08:02 -0700

    1.8.6 should use do

--- libraries/chef_rvm_set_helpers.rb
+++ libraries/chef_rvm_set_helpers.rb
@@ -3,7 +3,7 @@ class Chef
     module SetHelpers
       def rvm_do(user = nil)
         # Use Gem's version comparing code to compare the two strings
-        if Gem::Version.new(VersionCache.fetch_version(user)) <= Gem::Version.new("1.8.6")
+        if Gem::Version.new(VersionCache.fetch_version(user)) < Gem::Version.new("1.8.6")
           "exec"
         else
           "do"
