3f65b919e010e490bba8cd74e565f36df477ab4d
Fletcher Nichol <fnichol@nichol.ca>
2011-11-28 14:48:28 -0700

    Ensure that RVM version strings can be converted to RubyGems format.
    References #56

--- libraries/chef_rvm_set_helpers.rb
+++ libraries/chef_rvm_set_helpers.rb
@@ -3,7 +3,8 @@ class Chef
     module SetHelpers
       def rvm_do(user = nil)
         # Use Gem's version comparing code to compare the two strings
-        if Gem::Version.new(VersionCache.fetch_version(user)) < Gem::Version.new("1.8.6")
+        rvm_version = VersionCache.fetch_version(user).gsub(/(-|_|#)/, '.')
+        if Gem::Version.new(rvm_version) < Gem::Version.new("1.8.6")
           "exec"
         else
           "do"
