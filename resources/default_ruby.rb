ad578d25be99d0f51d9323edc37f41224dff48c5
Shaun Dern <shaun@substantial.com>
2012-04-05 14:52:56 -0700

    adding patch into resources

--- resources/default_ruby.rb
+++ resources/default_ruby.rb
@@ -23,6 +23,7 @@ actions :create
 
 attribute :ruby_string, :kind_of => String, :name_attribute => true
 attribute :user,        :kind_of => String
+attribute :patch,       :kind_of => String
 
 def initialize(*args)
   super
--- resources/ruby.rb
+++ resources/ruby.rb
@@ -23,6 +23,7 @@ actions :install, :uninstall, :remove
 
 attribute :ruby_string, :kind_of => String, :name_attribute => true
 attribute :user,        :kind_of => String
+attribute :patch,       :kind_of => String
 
 def initialize(*args)
   super
