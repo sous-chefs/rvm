425e319bb90dc04443c8b4cba8ecc2bdd02aea2b
Fletcher Nichol <fnichol@nichol.ca>
2013-01-15 00:45:24 -0700

    Update default Ruby to ruby-1.9.3-p327.

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,8 +20,8 @@
 #
 
 # ruby that will get installed and set to `rvm use default`.
-default['rvm']['default_ruby']      = "ruby-1.9.3-p194"
-default['rvm']['user_default_ruby'] = "ruby-1.9.3-p194"
+default['rvm']['default_ruby']      = "ruby-1.9.3-p327"
+default['rvm']['user_default_ruby'] = "ruby-1.9.3-p327"
 
 # list of additional rubies that will be installed
 default['rvm']['rubies']      = []
