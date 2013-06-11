b7f16f8fab50667cf3504750ba98c795e5f159bf
Ravil Bayramgalin <brainopia@evilmartians.com>
2013-06-11 16:19:37 +0400

    Add gawk as a dependency (latest stable rvm requires it for 2.0.0-p195)

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -132,7 +132,8 @@ def install_ruby_dependencies(rubie)
         pkgs  = %w{ build-essential openssl libreadline6 libreadline6-dev
                     zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
                     sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
-                    ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+                    ncurses-dev automake libtool bison ssl-cert pkg-config
+                    libgdbm-dev libffi-dev gawk }
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
         pkgs = %w{ gcc-c++ patch zlib zlib-devel libffi-devel
