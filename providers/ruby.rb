7f8081771d76d8fc9ff0044e7accd8ae44820707
Brian Hahn <brian.hahn@crowdflower.com>
2013-08-06 21:18:16 -0700

    packages are named differently in 10.04

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -131,8 +131,13 @@ def install_ruby_dependencies(rubie)
       when "debian","ubuntu"
         pkgs  = %w{ build-essential openssl libreadline6 libreadline6-dev
                     zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
-                    sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
-                    ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+                    sqlite3 libxml2-dev autoconf libc6-dev
+                    automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+        if node['platform_version'] == "10.04"
+          pkgs += %w{ libxslt1-dev libncurses5-dev }
+        else
+          pkgs += %w{ libxslt-dev ncurses-dev }
+        end
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
         pkgs = %w{ gcc-c++ patch zlib zlib-devel libffi-devel
