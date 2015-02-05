faaccae7e1d0649a172ac6efca0dcd8ef7cf4606
Faizal Zakaria <phaibusiness@gmail.com>
2015-02-05 16:59:45 +0800

    Install ruby by default for system recipe
    - As mentioned in the README, recipe[rvm::system] will install rvm and ruby.

--- recipes/system.rb
+++ recipes/system.rb
@@ -19,15 +19,10 @@
 
 include_recipe "rvm::system_install"
 
-perform_install_rubies  = node['rvm']['install_rubies'] == true ||
-                  node['rvm']['install_rubies'] == "true"
-
-if perform_install_rubies
-  install_rubies  :rubies => node['rvm']['rubies'],
-                  :default_ruby => node['rvm']['default_ruby'],
-                  :global_gems => node['rvm']['global_gems'],
-                  :gems => node['rvm']['gems']
-end
+install_rubies :rubies => node['rvm']['rubies'],
+               :default_ruby => node['rvm']['default_ruby'],
+               :global_gems => node['rvm']['global_gems'],
+               :gems => node['rvm']['gems']
 
 # add users to rvm group
 group 'rvm' do
