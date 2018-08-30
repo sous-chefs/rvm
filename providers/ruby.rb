de93c304d54ee51cc044b57ca6173b5b6ac87027
Jesús Eduardo Clemens Chong <edu.clemens@gmail.com>
2018-08-30 14:05:52 -0700

    Updating required packages for Ubuntu 16.10 or greater

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -129,10 +129,14 @@ def install_ruby_dependencies(rubie)
   when /^ruby-/, /^ree-/, /^rbx-/, /^kiji/
     case node['platform']
       when "debian","ubuntu"
-        pkgs  = %w{ build-essential openssl libreadline6 libreadline6-dev
-                    zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
-                    sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
+        pkgs  = %w{ build-essential openssl zlib1g zlib1g-dev libssl-dev libyaml-dev
+                    libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
                     ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+        if node['platform_version'].to_f >= 16.10
+          pkgs += %w{ libreadline libreadline-dev }
+        else
+          pkgs += %w{ libreadline6 libreadline6-dev }
+        end
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
         pkgs = %w{ gcc-c++ patch zlib zlib-devel libffi-devel
